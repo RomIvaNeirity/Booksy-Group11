470809c4deb0855d7882f39bf9dd8c55b55e5e3d
Roman <neirity@gmail.com>
2025-10-10 15:04:39 +0300

    fix swiper buttons

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,41 +1,22 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, Keyboard  } from 'swiper/modules';
+
 
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 export const heroSwiper = new Swiper('.hero-swiper', {
   modules: [Navigation],
-  speed: 600,
+    speed: 600,
+    loop: false,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true
+    },
   slidesPerView: 1,
   spaceBetween: 20,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  }, on: {
-    init() {
-      updateButtons(this);
-    },
-    slideChange() {
-      updateButtons(this);
-    },
-  },
+  }
 });
-
-function updateButtons(swiper) {
-  const prevBtn = document.querySelector('.swiper-button-prev');
-  const nextBtn = document.querySelector('.swiper-button-next');
-
-  if (swiper.isBeginning) {
-    prevBtn.classList.add('swiper-button-disabled');
-  } else {
-    prevBtn.classList.remove('swiper-button-disabled');
-  }
- 
-  if (swiper.isEnd) {
-    nextBtn.classList.add('swiper-button-disabled');
-  } else {
-    nextBtn.classList.remove('swiper-button-disabled');
-  }
-}
-;
