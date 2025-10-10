2a5bc3d4d2805fe680579c07d77df7bc8a7b5e91
ValeriiaM <moskalenkovvy@gmail.com>
2025-10-10 19:59:08 +0300

    add part of styles, add eventListeners

--- src/js/book.js
+++ src/js/book.js
@@ -3,25 +3,55 @@ import 'accordion-js/dist/accordion.min.css';
 
 import BooksAPI from './books-api';
 
-// const bookList = document.querySelector('.books-list');
+const bookModal = document.querySelector('.book-modal-layer');
+const bookDecsription = document.querySelector('.book-desc');
 const bookImg = document.querySelector('.book-img-container');
-const bookDetails = document.querySelector("[data-category='details'");
+const bookDetails = document.querySelector("[data-category='details']");
+const bookClose = document.querySelector('.btn-close-modal');
 
 async function showBook(bookId) {
   try {
     const bookData = await BooksAPI.fetchBookById(bookId);
     console.log(bookData);
     renderBook(bookData);
+    bookModal.classList.add('is-opened');
+
+    bookModal.addEventListener('click', closeModal);
+    document.addEventListener('keydown', closeModal);
   } catch (error) {
     console.log(error);
     return;
   }
 }
 
-function renderBook({ book_image, description }) {
+function renderBook({ book_image, title, author, price, description }) {
   bookImg.innerHTML = `<img class="book-img" src="${book_image}" alt="">`;
   if (description.length) bookDetails.textContent = description;
+  bookDecsription.insertAdjacentHTML(
+    'afterbegin',
+    `
+        <h3 class="book-title">${title}</h3>
+        <p class="book-author">${author}</p>
+        <p class="book-price">${price}</p>`
+  );
 }
+
+function closeModal(event) {
+  console.log(event.code);
+  if (
+    event.target.classList.contains('btn-close-modal') ||
+    event.target.classList.contains('book-modal-layer') ||
+    event.code === 'Escape'
+  ) {
+    bookModal.classList.remove('is-opened');
+    bookModal.removeEventListener('click', closeModal);
+    document.removeEventListener('keydown', closeModal);
+  }
+}
+
+bookModal.addEventListener('scroll', event => {
+  event.stopPropagation();
+});
 
 export function onBookClick(event) {
   if (event.target.nodeName !== 'BUTTON') {
