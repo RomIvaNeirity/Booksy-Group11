4eb0e3fe7af2356189271a38b23e806b8c1e63e0
Roman <neirity@gmail.com>
2025-10-10 12:51:37 +0300

    add hero layers

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -12,5 +12,30 @@ export const heroSwiper = new Swiper('.hero-swiper', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  }, on: {
+    init() {
+      updateButtons(this);
+    },
+    slideChange() {
+      updateButtons(this);
+    },
   },
 });
+
+function updateButtons(swiper) {
+  const prevBtn = document.querySelector('.swiper-button-prev');
+  const nextBtn = document.querySelector('.swiper-button-next');
+
+  if (swiper.isBeginning) {
+    prevBtn.classList.add('swiper-button-disabled');
+  } else {
+    prevBtn.classList.remove('swiper-button-disabled');
+  }
+ 
+  if (swiper.isEnd) {
+    nextBtn.classList.add('swiper-button-disabled');
+  } else {
+    nextBtn.classList.remove('swiper-button-disabled');
+  }
+}
+;
