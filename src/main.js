2c0f1d70441b47ac82a9289b304ab3dd707157de
Olha_Khodak <olgapolina261111@ukr.net>
2025-10-14 13:52:25 +0300

    fix errors, add modal-contacts.js

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ import { heroSwiper } from './js/swiper.js';
 import { swiperEvents } from './js/swiper.js';
 import { initMobileMenu } from './js/header.js';
 initMobileMenu();
+
+import { initModalContacts } from './js/modal-contacts.js';
+initModalContacts();
